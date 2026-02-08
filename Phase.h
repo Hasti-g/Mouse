0cd7d7b2447fecfcc1eea33be250c86b038b4954
Bahar <masoumibahar2@gmail.com>
2026-02-08 15:09:41 +0330

    Update Phase.h

--- Phase.h
+++ Phase.h
@@ -1,2 +1,23 @@
 #pragma once
-class 
+#include "Stage.h"
+#include <vector>
+
+class Phase
+{
+private:
+    std::vector<Stage> stages;
+    int currentStage;
+    bool phaseEnded;
+    bool phaseWon;
+
+public:
+    Phase(bool isPhase2 = false);
+
+    void Start();
+    void ProcessHit(int x, int y);
+    void Update();
+
+    bool IsEnded() const;
+    bool IsWon() const;
+    int GetTotalScore() const;
+};

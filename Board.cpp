58a0b8622d29a01f45259ce4a86fca1af148a8a8
Bahar <masoumibahar2@gmail.com>
2026-02-08 15:07:42 +0330

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -1,23 +1,25 @@
-#include"Board.h"
+#include "Board.h"
 
-Board::Board(int w, int l) : width(w), length(l), moleVisible(l, std::vector<bool>(w, false)) {}
+Board::Board(int w, int l)
+    : width(w), length(l), moleVisible(l, std::vector<bool>(w, false)) {
+}
 
 void Board::Show(int x, int y)
 {
-	if (x >= 0 && x < width && y >= 0 && y < length)
-		moleVisible[y][x] = true;
+    if (x >= 0 && x < width && y >= 0 && y < length)
+        moleVisible[y][x] = true;
 }
 
-void Board::Disappear()
+void Board::Hide()
 {
-	for (int y = 0; y < length; y++)
-		for (int x = 0; x < width; x++)
-			moleVisible[y][x] = false;
+    for (int y = 0; y < length; y++)
+        for (int x = 0; x < width; x++)
+            moleVisible[y][x] = false;
 }
 
-bool Board::IsMoleAt(int x, int y)
+bool Board::IsMoleAt(int x, int y) const
 {
-	if (x < 0 || x >= width || y < 0 || y >= length)
-		return false;
-		return moleVisible[y][x];
+    if (x < 0 || x >= width || y < 0 || y >= length)
+        return false;
+    return moleVisible[y][x];
 }
